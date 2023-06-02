06e1931c5efcd04286e0f3b0b62e5b6284138dee
Santiago Caruso <127801059+Santiicc@users.noreply.github.com>
2023-06-02 14:28:11 -0300

    Add files via upload

---
+++ src/Library/UserManager.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+
+namespace ClassLibrary;
+
+public class UserManager
+{
+
+    private List<User>users;
+    public void deleteuser()
+    {
+        
+    }
+
+    public void registerUser()
+    {
+
+    }
+}
